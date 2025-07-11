47ea59b2510e7a1aea85dad88b42714a1509f41a
William SIMON--VEZO <william.simonvezo@gmail.com>
2025-07-11 21:22:22 +0200

    first commit

---
+++ backend/hardhat.config.ts
@@ -0,0 +1,29 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'solidity-docgen';
+
+const {
+  ETHERSCAN_API_KEY = '',
+  PRIVATE_KEY = '',
+} = process.env;
+
+const config: HardhatUserConfig = {
+  docgen: { outputDir: 'doc' },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        passphrase: PRIVATE_KEY,
+      },
+      chainId: 31337,
+    },
+  },
+  solidity: '0.8.20',
+};
+
+export default config;
