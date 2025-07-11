dc15684ab97c337f76dd95cb4c5a088354faba1e
saimeunt <saimeunt@gmail.com>
2025-07-11 21:33:25 +0200

    update frontend libs

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /*webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push('pino-pretty', 'encoding');
     return config;
-  },
+  },*/
 };
 
 export default nextConfig;
